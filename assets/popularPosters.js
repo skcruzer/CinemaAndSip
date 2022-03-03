23d119aba38cc6e4b9cf78b200f3d9ffa1233466
Shaun <34657716+ST12345678910@users.noreply.github.com>
2022-03-03 10:16:38 -0800

    daily drink/movie suggestions working:
    - random drink url used to put img of random drink in daily suggestion.
    - random movie suggestion is picked randomly from list of currently popular movies

--- assets/popularPosters.js
+++ assets/popularPosters.js
@@ -2,6 +2,8 @@
 // this constant is what will be changed in index.html
 const homeScreenbg = document.getElementById('movies')
 
+const suggestedMovie = document.getElementById('suggestedMovie')
+
 //key must be set as a const, base urls for whatever information is needed should be placed below
 
 const config = {
@@ -14,8 +16,12 @@ const BASE_URL = config.api_base_url
 const API_KEY = config.api_key
 
 //takes the first page of currently popular movies
+//chooses randomly from the first 12 pages of popular movies, displays one poster at a time, any more than 12 pages begins to have longer load times
 
-export async function currentPopular(page = 1) {
+let randomPopularPage = Array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)
+randomPopularPage = [Math.floor(Math.random() * randomPopularPage.length + 1)];
+
+export async function currentPopular(page = randomPopularPage) {
   let data = []
   try {
     const response = await fetch(`${BASE_URL}movie/popular?api_key=${API_KEY}&page=${page}`)
@@ -32,17 +38,33 @@ export async function currentPopular(page = 1) {
 export async function presentPosters() {
   const movies = await currentPopular()
   console.log(homeScreenbg)
-  homeScreenbg.innerHTML = movies?.map(movie => singlePoster(movie)).join("")
+  homeScreenbg.innerHTML = movies?.map((movie) => singlePoster(movie)).join("")
+
+// movie suggestion is also taken from popular movies, a movie is randomly selected from the 12 movies that are loaded every time
+
+  suggestedMovie.innerHTML = movies?.map((movie) => singleMovsuggestion(movie))[randomPopularPage]
+  console.log(suggestedMovie)
 }
 
 function singlePoster(movie) {
   return (
     `
-        
             <img src="${config.image_base_url + movie?.poster_path}" id="posterBack" class="img-fluid">
-        
         `
+        
   )
+
+//returns a single image for the suggestion, but with a different div than the background posters, this image can have it's own animations/effects
+
+}
+function singleMovsuggestion(movie) {
+  return (
+    `
+            <img src="${config.image_base_url + movie?.poster_path}" id="suggestionPoster" class="img-fluid">
+        `
+
+  )
+
 }
 
 //this will run when the page is loaded
