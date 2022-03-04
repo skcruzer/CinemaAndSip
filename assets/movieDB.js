e262ab2b95af79d8838e879fe0edbc02d2ced587
Shaun <34657716+ST12345678910@users.noreply.github.com>
2022-03-04 13:10:05 -0800

    added MOVIEDB functionality; check notes
    daily suggestion auto generated, name of movie underneat, and date released underneath. Playing around with search function, currently the search function delivers the most relevant results per keyword

--- assets/movieDB.js
+++ assets/movieDB.js
@@ -58,23 +58,82 @@ function singlePoster(movie) {
 
 }
 function singleMovsuggestion(movie) {
+  console.log(config.api_base_url + movie.original_title)
   return (
     `
             <img src="${config.image_base_url + movie?.poster_path}" id="suggestionPoster" class="img-fluid">
+            <div id="" style="font-size: 24px">${movie.original_title}</div>
+            <div id="" style="font-size: 24px">${movie.release_date}</div>
         `
 
   )
 
 }
-
-
-
-
-
+document.querySelector('form.form').addEventListener('submit', function (e) {
+  e.preventDefault();
+  let x = document.querySelector('form.form').elements;
+  console.log("movieUserInput: ", x['movieInput'].value);
+  const userMovieinput = (x['movieInput'].value);
+  document.getElementById('movieSearchbar').reset();
+  
+  
+  var movieMultisearch = "https://api.themoviedb.org/3/search/movie?api_key=8bb7ff26c787b44f4d7c77a6ef8dfb70&language=en-US&page=1&include_adult=false&query=" + userMovieinput
+  const movieRecbox1 = document.getElementById('movieRec1')
+  const movieRecbox2 = document.getElementById('movieRec2')
+  const movieRecbox3 = document.getElementById('movieRec3')
+  const movieRecbox4 = document.getElementById('movieRec4')
+  const movieRecbox5 = document.getElementById('movieRec5')
+  fetch(movieMultisearch)
+    .then(
+      (response) => {
+        if (response.status !== 200) {
+          console.log("looks like there was a problem")
+          return
+        }
+        response.json().then(function (data) {
+          movieRecbox1.innerHTML = 
+        `
+        <img src="${config.image_base_url + data.results[0].backdrop_path}" id="userMovieposter1" class="img-fluid">
+        <div id="userMovieoptions1" style="font-size: 24px">${data.results[0].original_title}</div>
+        <div id="userMovieoptions1date" style="font-size: 10px">${data.results[0].release_date}</div>
+        `
+          movieRecbox2.innerHTML =
+            `
+        <img src="${config.image_base_url + data.results[1].backdrop_path}" id="userMovieposter2" class="img-fluid">
+        <div id="userMovieoptions2" style="font-size: 24px">${data.results[1].original_title}</div>
+        <div id="userMovieoptions2date" style="font-size: 10px">${data.results[1].release_date}</div>
+        `
+          movieRecbox3.innerHTML =
+            `
+        <img src="${config.image_base_url + data.results[2].backdrop_path}" id="userMovieposter3" class="img-fluid">
+        <div id="userMovieoptions3" style="font-size: 24px">${data.results[2].original_title}</div>
+        <div id="userMovieoptions3date" style="font-size: 10px">${data.results[2].release_date}</div>
+        `
+          movieRecbox4.innerHTML =
+            `
+        <img src="${config.image_base_url + data.results[3].backdrop_path}" id="userMovieposter4" class="img-fluid">
+        <div id="userMovieoptions4" style="font-size: 24px">${data.results[3].original_title}</div>
+        <div id="userMovieoptions4date" style="font-size: 10px">${data.results[3].release_date}</div>
+        `
+          movieRecbox5.innerHTML =
+            `
+        <img src="${config.image_base_url + data.results[4].backdrop_path}" id="userMovieposter5" class="img-fluid">
+        <div id="userMovieoptions5" style="font-size: 24px">${data.results[4].original_title}</div>
+        <div id="userMovieoptions5date" style="font-size: 10px">${data.results[4].release_date}</div>
+        `
+        console.log(data.results)
+          console.log(data.results[0].original_title)
+          console.log(data.results[1].original_title)
+        })
+      }
+    )
+    .catch(function (err) {
+      console.log("fetch error," + err);
+    });
+});
 
 
 //this will run when the page is loaded
-
 function App() {
   presentPosters()
 }
--- assets/script.js
+++ assets/script.js
@@ -278,3 +278,4 @@ function getDrink(suggestedUrl){
   getIngr(suggestedUrl);
   console.log(drinkIngr)
 }
+
